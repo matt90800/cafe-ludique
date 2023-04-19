93f588655b85799146329228461c2fcab9b7fdc3
matt90800 <matthieu_balland@hotmail.fr>
2023-04-19 17:06:15 +0200

    trois pages préparées, fetch non fonctionel à cause du foreach

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-fetch("https://10.113.28.69:8443/data.json")
+fetch("https://localhost:8443/data.json")
     .then(response => response.json())
-    .then(msg=>(console.log(msg)))
+    .then(json=>{
+        json.forEach(
+            element => {
+                createGameCard(element);
+            }
+        );
+            console.log(json);
+        }        
+    );
+
+const main = document.querySelector("main");
+const createGameCard = (element)=>{
+    
+    console.log(element);
+    element=element.games;
+    const card = document.createElement("div");
+    card.className="card col-4";
+    card.innerHTML=
+    `
+    <h5 class="card-title">${element.title}</h5>
+    <img class="card-img-top" src=${element.image} alt="${element.image}">
+      <p> ${element.info}</p>
+      <p> ${element.year}</p>
+      <p> ${element.year}</p>
+      <p> ${element.year}</p>
+      <p> ${element.year}</p>
+      <p> ${element.year}</p>
+    `;
+    main.append(card);
+}
